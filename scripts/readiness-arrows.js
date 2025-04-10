321d192534195211385c3181625b36566647349f
Phoenix-Technoprojects <advtech@phoenix-technoprojects.com>
2025-04-10 09:21:17 +0100

    Update readiness-arrows.js

--- scripts/readiness-arrows.js
+++ scripts/readiness-arrows.js
@@ -134,9 +134,9 @@ const data = [
   }
 ];
 
-const margin = { top: 50, right: 50, bottom: 50, left: 50 },
+const margin = { top: 50, right: 50, bottom: 10, left: 50 },
   width = 700 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  height = 460 - margin.top - margin.bottom;
 
 const svg = d3
   .select("#readiness-arrows")
